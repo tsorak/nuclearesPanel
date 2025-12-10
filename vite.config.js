60d8649af2ec5d10ffe0290919281b7bec5052b4
Karl Osterberg <karost0249@gmail.com>
2025-12-10 23:11:00 +0100

    Bundle JS and CSS into html for ease of use

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [solid(), tailwindcss()],
+  plugins: [solid(), tailwindcss(), viteSingleFile()],
   server: {
     port: 3000,
     watch: ["src/**/*"],
